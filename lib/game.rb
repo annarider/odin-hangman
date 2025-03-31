4a77d3d4cd0bd740d498dfb0317d957da539cfa6
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-31 14:15:57 -0600

    Add: Game class with skeleton to begin logic

---
+++ lib/game.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# main contains the main execution
+# for the tic-tac-toe game.
+#
+# @example Create a new Game 
+# game = Game.new
+require_relative 'secret'
+
+class Game
+  NUMBER_OF_ROUNDS = 12
+  
+  attr_accessor :word, :board, :state, :interface
+
+  def initialize
+    
+  end
+end
