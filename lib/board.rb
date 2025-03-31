bdc014aeaa6113c3a21e2b3e6c192dc3cc55b1c0
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-31 14:25:35 -0600

    Add: Board class with inst variables & init method

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Board controls the game rules
+# for the Hangman game. Its duties
+# include validating guesses and
+# providing feedback.
+#
+# @example Create a new Board 
+# board = Board.new
+class Board
+  require_relative 'secret'
+
+  NUMBER_OF_ROUNDS = 12
+  
+  attr_accessor :word, :guess_history, :current_letter
+
+  def initialize(word = nil, current_guess = nil)
+    @word = word
+    @guess_history = '_' * @word.length
+    @current_letter = current_guess
+  end
+end
