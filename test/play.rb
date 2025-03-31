c45d6aa01fe3c23ca43afb8d2028b426e1a013c4
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-03-31 12:20:59 -0600

    Add: Test play from Mastermind game to start
    Added a new file for testing game play logic.
    
    I used the Mastermind test play script as a
    starting point for this Hangman test.
    
    The script is a big broken for now.

---
+++ test/play.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+# Test Play tests Hangman
+# game play by allowing to set the 
+# secret word and guesses. It 
+# tests game logic, scoring, 
+# interface, etc.
+#
+require_relative '../lib/secret'
+require_relative '../lib/game'
+require_relative '../lib/board'
+require_relative '../lib/interface'
+require_relative '../lib/state'
+
+module TestPlay
+  class << self
+    attr_accessor :word, :board, :guess, :game
+  end
+
+  class MockInterface < Interface
+    def initialize(guesses)
+      @guesses = guesses
+      @current_guess_count = 0 
+    end
+  end
+
+  def self.setup
+    @word = Secret.word
+    @guess = ['a', 'e', 'i']
+    @board = Board.new(@word, @guess)
+    @game = Game.new
+  end
+
+  def self.test_board_setup
+    p "board: #{@board}"
+    p "check word guess: #{@board.win?(@word)}"
+    p "check random guess: #{@board.win?(@guess)}"
+  end
+
+  def self.test_guess_feedback
+    p "feedback: #{@board.feedback(@word)}"
+    p "feedback: #{@board.feedback(@guess)}"
+  end
+
+  def self.test_gameover
+    @board.current_guess = @word ## have Game class call to set guess
+    p "gameover with code? #{@board.game_over?}, #{@word}"
+    @board.current_guess = @guess
+    p "gameover with guess? #{@board.game_over?}, #{@guess}}"
+  end
+
+  def self.test_board_history
+    p "history: #{@board.history}"
+    p "hint: #{@board.hint}"
+  end
+
+  def self.test_interface
+    interface = Interface.new
+    interface.welcome
+    guess = interface.request_guess
+    p guess
+    p interface.valid?(guess)
+    interface.show_board(@board)
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  TestPlay.setup
+  # TestPlay.test_board_setup
+  # TestPlay.test_guess_feedback
+  # TestPlay.test_gameover
+  # TestPlay.test_board_history
+  TestPlay.test_interface
+end
+end
