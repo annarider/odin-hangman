b26596359b7472da34b67e83cc268bb2f1df47f7
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-01 09:27:42 -0600

    Add: Init method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -10,12 +10,16 @@
 # game = Game.new
 class Game
   require_relative 'secret'
-  
+
   NUMBER_OF_ROUNDS = 12
   
   attr_accessor :word, :board, :state, :interface
 
   def initialize
-    
+    @word = Secret.pick_word
+    @interface = Interface.new
+    guess = @interface.guess
+    @board = Board.new(@word, guess)
+    @state = State.new(NUMBER_OF_ROUNDS)
   end
 end
