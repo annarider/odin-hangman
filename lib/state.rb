2f54f512852ad1fabd4073a542489f84b2e1adfe
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-04-03 10:26:12 -0600

    Update: Following Rubocop's style fixes

--- lib/state.rb
+++ lib/state.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
-# Game State class is responsible for 
-# passively tracking the game's state 
+# Game State class is responsible for
+# passively tracking the game's state
 # and prepping to save the game.
-# It saves the state such as game_over and 
+# It saves the state such as game_over and
 # guess history. It's also responsible for
 # serializing & deserializing saved games.
 #
 # @example Create a new Game State
 # state = State.new
 class State
-
   NUMBER_OF_ROUNDS = 12
 
   attr_accessor :remaining_guesses, :guessed_letters
